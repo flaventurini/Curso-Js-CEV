3081e91d2376bfd9c3e521bc4a32762a1e4fe2db
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-04-10 14:11:42 -0300

    Curso JS

---
+++ Aula 08/ex005.js
@@ -0,0 +1,14 @@
+var corpo = window.document.body;
+var p1 = document.getElementsByTagName('p')[1];
+/*
+document.write('Está escrito assim: ' + p1.innerHTML);
+
+p1.style.color = 'blue'
+
+var d = document.getElementById('msg')
+d.style.background = 'green'
+d.innerText = 'Estou aguardando...'
+*/
+
+var d = document.querySelector('div#msg');
+d.style.color = 'blue';
