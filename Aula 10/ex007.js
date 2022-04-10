d5e0f2b07938a9c6c7e04fb343a832196ecf3b47
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-04-10 14:17:13 -0300

    Curso JS

--- Aula 10/ex007.js
+++ Aula 10/ex007.js
@@ -1,6 +1,6 @@
 function somar() {
-  var tn1 = document.querySelector('input#txtn1');
-  var tn2 = document.querySelector('input#txtn2');
+  var tn1 = document.querySelector('#txtn1');
+  var tn2 = document.querySelector('#txtn2');
   var res = document.getElementById('res');
   var n1 = Number(tn1.value);
   var n2 = Number(tn2.value);
