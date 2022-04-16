22ce94fa7fd89c6e8f8185488f98d2b88f4cfb75
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-04-16 20:50:40 -0300

    Updated

--- Aula 12 EX/Ex015/script.js
+++ Aula 12 EX/Ex015/script.js
@@ -1,3 +1,23 @@
 function verificar() {
-  // 8:18min
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fAno = document.getElementById('txtano')
+  var res = document.getElementById('res')
+  if (fAno.value.length == 0 || fAno.value > ano) {
+    alert('Verifique os dados e tente novamente!')
+  } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fAno.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    
+    if (fsex[0].checked) {
+      genero = 'Homem'
+    } else if (fsex[1].checked) {
+      genero = 'Mulher'
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+  }
 }
