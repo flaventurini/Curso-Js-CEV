84a1b2b1f63f9869dcc4b8dd0c9d779888d9ce87
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-04-16 20:58:23 -0300

    Updated

--- Aula 12 EX/Ex015/script.js
+++ Aula 12 EX/Ex015/script.js
@@ -11,13 +11,31 @@ function verificar() {
     var genero = ''
     var img = document.createElement('img')
     img.setAttribute('id', 'foto')
-    
     if (fsex[0].checked) {
       genero = 'Homem'
+      if (idade >= 0 && idade < 10) {
+        img.setAttribute('src', 'img/foto-bebe-m.png')
+      } else if (idade < 21) {
+        img.setAttribute('src', 'img/foto-jovem-m.png')
+      } else if (idade < 50) {
+        img.setAttribute('src', 'img/foto-adulto-m.png')
+      } else {
+        img.setAttribute('src', 'img/foto-idoso-m.png')
+      }
     } else if (fsex[1].checked) {
       genero = 'Mulher'
+      if (idade >= 0 && idade < 10) {
+        img.setAttribute('src', 'img/foto-bebe-f.png')
+      } else if (idade < 21) {
+        img.setAttribute('src', 'img/foto-jovem-f.png')
+      } else if (idade < 50) {
+        img.setAttribute('src', 'img/foto-adulto-f.png')
+      } else {
+        img.setAttribute('src', 'img/foto-idoso-f.png')
+      }
     }
     res.style.textAlign = 'center'
     res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
   }
 }
