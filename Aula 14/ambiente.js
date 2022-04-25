0ed1f65e87c1169700f12343cacf4b1de338cdc4
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-04-25 19:03:41 -0300

    Updated

--- Aula 14/ambiente.js
+++ Aula 14/ambiente.js
@@ -8,3 +8,14 @@ for (var c = 1; c <= 10; c++) {
 }
 
 console.log('FIM!')
+
+
+var d = 1
+var soma = d
+while (d <=5) {
+  var num = 1
+  var soma = soma + num
+  console.log(soma)
+  num++
+  d++ 
+}
