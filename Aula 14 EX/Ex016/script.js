cdca7b68b25d073606cca63d82b123445fb136ca
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-04-27 20:03:17 -0300

    Updated

--- Aula 14 EX/Ex016/script.js
+++ Aula 14 EX/Ex016/script.js
@@ -1,15 +1,30 @@
 function contar() {
-  var inicio = document.getElementById('formInicio')
-  var fim = document.getElementById('formFim')
-  var passo = document.getElementById('formPasso')
-  var res = document.getElementById('res')
-  
-  while (inicio < Number.parseInt(fim)) {
-    inicio = Number.parseInt(inicio)
-    inicio = inicio + Number.parseInt(passo)
+  let inicio = document.getElementById('txti')
+  let fim = document.getElementById('txtf')
+  let passo = document.getElementById('txtp')
+  let res = document.getElementById('res')
 
+  if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    alert('[ERRO] Faltam dados!')
+  } else {
+    res.innerHTML = 'Contando: '
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    
+    if (i < f) {
+      // Contagem crescente
+      for(let c = 1; c <= f; c += p) {
+        res.innerHTML += ` ${c} \u{1F449}`
+      }
+
+    } else {
+      // Contagem regressiva
+      for(let c = 1; c >= f; c -= p) {
+        res.innerHTML += ` ${c} \u{1F449}`
+      }
+      
+    }
+    res.innerHTML += `\u{1F3C1}`
   }
-  
-  
-  res.innerHTML = `Detectamos ${inicio} com ${fim} anos.`
 }
