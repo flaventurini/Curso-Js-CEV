8500932a8ef9bbeaaec6215d31b8538c36679407
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-04-29 21:16:29 -0300

    Updated

--- Aula 14 EX/Ex017/script.js
+++ Aula 14 EX/Ex017/script.js
@@ -1,4 +1,15 @@
-function tabuada() {
+function calcular() {
   let num = document.getElementById('num')
- // inserir operação switch
+  let n = Number(num.value)
+  let mult = 1
+
+  while(mult <= 10) {
+    let res = document.getElementById('res')
+    res.innerHTML = `${n} x ${mult} = ${n*mult}</br>`
+    mult++
+  }
+  
+  //for(let mult = 1; mult <= 10; mult++) {
+    //res.innerHTML = `${n} x ${mult} = ${n*mult}</br>`
+  //}
 }
