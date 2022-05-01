5dae7748b39f627f6184286e4b32ab51d19bc36b
Flávia Venturini <100423453+flaventurini@users.noreply.github.com>
2022-04-30 22:39:07 -0300

    Add FOR instead of WHILE

--- Aula 14 EX/Ex017/script.js
+++ Aula 14 EX/Ex017/script.js
@@ -6,14 +6,28 @@ function calcular() {
     alert('Por favor, digite um número!')
   } else {
     let n = Number(num.value)
-    let c = 1
+    
     tab.innerHTML = ''
-    while (c <= 10) {
+    
+    /* Opção de repetição com WHILE 
+    
+    let c = 1
+    
+      while (c <= 10) {
       let item = document.createElement('option')
       item.text = `${n} x ${c} = ${n*c}`
       item.value = 'tab$(c)'
       tab.appendChild(item)
       c++
+    } */
+
+    // Opção de repetição com FOR
+
+    for (let c = 1; c <= 10; c++) {
+      let item = document.createElement('option')
+      item.text = `${n} x ${c} = ${n*c}`
+      item.value = 'tab$(c)'
+      tab.appendChild(item)
     }
     }
   
