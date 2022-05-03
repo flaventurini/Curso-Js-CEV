84fe32d85291f3ff21849eba41ce14ace62797d1
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-05-03 17:42:14 -0300

    Updated

--- Aula 15/Ex018/vetor_na_tela.js
+++ Aula 15/Ex018/vetor_na_tela.js
@@ -1,5 +1,16 @@
 let valores = [8, 1, 7, 4, 2, 9]
 
+// primeira opção, clássica
+
 for (let i = 0; i < valores.length; i++) {
   console.log(`A posição ${i} tem o valor ${valores[i]}`)
+}
+
+
+// segunda opção, mais moderna
+
+let num = [8, 1, 7, 4, 2, 9]
+
+for (let j in num) {
+  console.log(`A posição ${j} tem o valor ${num[j]}`)
 }
