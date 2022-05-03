98c07541a5a4400bcd927dd4c3d868550cd89963
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-05-03 17:20:14 -0300

    Updated

---
+++ Aula 15/Ex018/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3]
+num[3] = 6
+num.push(10)
+
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+num.sort()
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} elementos`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
