3295f37a89176fbc0042c80abca529f2c36b0378
flaventurini <100423453+flaventurini@users.noreply.github.com>
2022-05-10 23:04:12 -0300

    Updated

--- Aula 16 EX/Ex018/script.js
+++ Aula 16 EX/Ex018/script.js
@@ -25,7 +25,21 @@ function adicionar() {
     let item = document.createElement('option')
     item.text = `Valor ${num.value} adicionado.`
     lista.appendChild(item)
+    res.innerHTML = ''
   } else {
     alert('Valor inválido ou já encontrado na lista.')
+  }
+  num.value = ''
+  num.focus()
+}
+
+function finalizar() {
+  if (valores.length == 0) {
+    alert('Adicione valores antes de finalizar!')
+  } else {
+    let tot = valores.length
+
+    res.innerHTML = ''
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
   }
 }
